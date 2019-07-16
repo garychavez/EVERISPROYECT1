6f335340a162d65ae9603375d39f4ff143e329b9
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-16 14:31:37 -0500

    EurekaClient

--- src/main/java/com/everis/EverisProyectApplication.java
+++ src/main/java/com/everis/EverisProyectApplication.java
@@ -2,8 +2,10 @@ package com.everis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EverisProyectApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/everis/pruebas/Pruebas.java
+++ src/test/java/com/everis/pruebas/Pruebas.java
@@ -2,4 +2,5 @@ package com.everis.pruebas;
 
 public class Pruebas {
 
+	int s;
 }
